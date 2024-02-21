a564e4226b7a3dbb7e3a31dd73f9125f8874b021
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2024-02-21 16:06:52 -0500

    Update deprecated method call

--- src/test/suite/file-controller.test.ts
+++ src/test/suite/file-controller.test.ts
@@ -13,16 +13,16 @@ suite('Extension Test Suite', () => {
     vscode.window.showInformationMessage('Start all tests.');
 
     test("unique Array test", () => {
-        assert.deepEqual((fileController as any).uniqueArray(["hoge", "hoge", "foo", "bar"]), ["hoge", "foo", "bar"]);
+        assert.deepStrictEqual((fileController as any).uniqueArray(["hoge", "hoge", "foo", "bar"]), ["hoge", "foo", "bar"]);
     });
 
     test("Get parent path recursive test", () => {
         let paths = (fileController as any).getParentPathRecursive("/hoge/test/bar", "/hoge");
-        assert.deepEqual(paths, ["/hoge/test/bar", "/hoge/test"]);
+        assert.deepStrictEqual(paths, ["/hoge/test/bar", "/hoge/test"]);
     });
 
     test("Flatten Array test", () => {
-        assert.deepEqual((fileController as any).flatten([
+        assert.deepStrictEqual((fileController as any).flatten([
                 ["hoge", "foo", "bar"],
                 ["hoge", "hoge"],
             ]),
@@ -32,7 +32,7 @@ suite('Extension Test Suite', () => {
 
     test("Get parent path recursive array test", () => {
         let paths = (fileController as any).getParentPathArrayRecursive(["/hoge/test/bar", "/hoge/test/foo/bar"], "/hoge");
-        assert.deepEqual(paths, ["/hoge/test/bar", "/hoge/test", "/hoge/test/foo/bar", "/hoge/test/foo"]);
+        assert.deepStrictEqual(paths, ["/hoge/test/bar", "/hoge/test", "/hoge/test/foo/bar", "/hoge/test/foo"]);
     });
 });
 
